a69e865475dbb8803d9e4b2b0f4c7046e7a55838
Kirill Shevtsov <kirwork.em@gmail.com>
2020-02-08 18:49:35 +0200

    Updates

---
+++ precache-manifest.90efffebfcc3dd3f0fd33fa51625bd0e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fdc7697234854dc92e2b8a8b8963ce3",
+    "url": "/sendsay-test-app/index.html"
+  },
+  {
+    "revision": "23a617216074fca81996",
+    "url": "/sendsay-test-app/static/css/main.b6e9e667.chunk.css"
+  },
+  {
+    "revision": "3151ff60674676e420f9",
+    "url": "/sendsay-test-app/static/js/2.2ec823b6.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/sendsay-test-app/static/js/2.2ec823b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23a617216074fca81996",
+    "url": "/sendsay-test-app/static/js/main.4bdbce9d.chunk.js"
+  },
+  {
+    "revision": "3567803954cddf785671",
+    "url": "/sendsay-test-app/static/js/runtime-main.77220395.js"
+  },
+  {
+    "revision": "bb549f9347cda4460db48a6c1dbd936a",
+    "url": "/sendsay-test-app/static/media/LOGO.bb549f93.svg"
+  },
+  {
+    "revision": "79672ab9198fb5cd5d61c5e3af5fb385",
+    "url": "/sendsay-test-app/static/media/SFProDisplay-Regular.79672ab9.woff"
+  },
+  {
+    "revision": "f51774821b5274771e2b637f7c278abf",
+    "url": "/sendsay-test-app/static/media/SFProDisplay-Regular.f5177482.woff2"
+  },
+  {
+    "revision": "85f937d4e7e5aea82be7697400a789bf",
+    "url": "/sendsay-test-app/static/media/icon-clip-big.85f937d4.svg"
+  },
+  {
+    "revision": "708d28270ec47b3dc6c0734d9f947ce3",
+    "url": "/sendsay-test-app/static/media/icon-clip.708d2827.svg"
+  },
+  {
+    "revision": "dc90b2f6d4b009da33b6474fdab6ac82",
+    "url": "/sendsay-test-app/static/media/icon-trash.dc90b2f6.svg"
+  },
+  {
+    "revision": "43ccd05e024c89775f099cb82b8931e2",
+    "url": "/sendsay-test-app/static/media/icon-warning.43ccd05e.svg"
+  }
+]);
