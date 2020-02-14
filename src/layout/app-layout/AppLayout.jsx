05d34c317e602691a91dacc1c232cb289f5690dd
Kirill Shevtsov <kirwork.em@gmail.com>
2020-02-14 23:59:51 +0200

    Fix: preventing the browser from loading the file if it was outside the File receiver

--- src/layout/app-layout/AppLayout.jsx
+++ src/layout/app-layout/AppLayout.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react';
 import Icon from '../../components/icon/Icon';
 import MessageSendFormContainer from '../../containers/MessageSendFormContainer';
 import SendedMessagesListContainer from '../../containers/SendedMessagesListContainer';
@@ -6,19 +7,39 @@ import SendedMessagesListContainer from '../../containers/SendedMessagesListCont
 import './AppLayout.css';
 import LogoIcon from '../../icons/LOGO.svg';
 
-function AppLayout() {
-  return (
-    <div className="AppLayoutWrapper">
-      <Icon src={LogoIcon} alt="App logo" className="AppLogo" />
-      <div className="u-marginBottom">
-        <MessageSendFormContainer />
+class AppLayout extends Component {
+  constructor(props) {
+    super(props);
+    this.preventFileDrop = this.preventFileDrop.bind(this);
+    this.preventFileDragover = this.preventFileDragover.bind(this);
+  }
+
+  preventFileDrop(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  preventFileDragover(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  render() {
+    return (
+      <div className="AppLayoutWrapper" onDrop={this.preventFileDrop} onDragOver={this.preventFileDragover}>
+        <div className="AppLogoWrapper">
+          <Icon src={LogoIcon} alt="App logo" className="AppLogo" />
+        </div>
+        <div className="u-marginBottom">
+          <MessageSendFormContainer />
+        </div>
+        <section className="SendedMessagesInfo u-marginBottom">
+          <h2 className="Heading SendedMessagesInfo-mainHeading  u-letterSpacing">Отправленные сообщения</h2>
+          <SendedMessagesListContainer />
+        </section>
       </div>
-      <section className="SendedMessagesInfo u-marginBottom">
-        <h2 className="Heading SendedMessagesInfo-mainHeading  u-letterSpacing">Отправленные сообщения</h2>
-        <SendedMessagesListContainer />
-      </section>
-    </div>
-  );
+    );
+  }
 }
 
 export default AppLayout;
