3d592c46f9dccc2dcd25eb49cd862e924f795e63
Kirill Shevtsov <kirwork.em@gmail.com>
2020-02-18 14:05:26 +0200

    Delete convertFileListToArray.js

--- src/utilities/convertFileListToArray.js
+++
@@ -1,9 +0,0 @@
-function convertFileListToArray(fileList) {
-  const array = [];
-  for (let i = 0; i < fileList.length; i += 1) {
-    array.push(fileList[i]);
-  }
-  return array;
-}
-
-export default convertFileListToArray;
