914b70470e486c81a21e15ccd84a7981d76d845c
Kirill Shevtsov <kirwork.em@gmail.com>
2020-02-18 14:05:50 +0200

    Delete isEqual.js

--- src/utilities/isEqual.js
+++
@@ -1,7 +0,0 @@
-export const isObjectsEqual = (objFirst, objSecond) => (typeof objFirst === 'object' && Object.keys(objFirst).length > 0
-  ? Object.keys(objFirst).length === Object.keys(objSecond).length
-  && Object.keys(objFirst).every((key) => isObjectsEqual(objFirst[key], objSecond[key]))
-  : objFirst === objSecond);
-
-export const isArraysOfObjectsEqual = (arrFirst, arrSecond) => arrFirst.length === arrSecond.length
-  && arrFirst.every((element, index) => isObjectsEqual(element, arrSecond[index]));
